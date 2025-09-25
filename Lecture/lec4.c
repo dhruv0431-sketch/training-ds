182025b7f5b4ffda7a73a094b362bcbc47d66556
DHRUV PANDEY <panditdhruv0431@gmail.com>
2025-09-25 13:19:00 +0530

    new

--- Lecture/lec4.c
+++ Lecture/lec4.c
@@ -1,76 +1,76 @@
-#include<stdio.h>
-// int main(){ 
-//     int x=5; 
-//     int y=10;  
-//      (&x , &y); 
+// #include<stdio.h>
+// // int main(){ 
+// //     int x=5; 
+// //     int y=10;  
+// //      (&x , &y); 
      
-//    x=x+y;
-//    y=x-y; 
-//    x=x-y; 
-
-//    printf("%d// %d",x,y);
+// //    x=x+y;
+// //    y=x-y; 
+// //    x=x-y; 
+// new
+// //    printf("%d %d",x,y);
     
-// } 
+// // } 
    
-// int main(){ 
-//     int arr[4]={1,2,3,4}; 
-//     int *p=arr;  
+// // int main(){ 
+// //     int arr[4]={1,2,3,4}; 
+// //     int *p=arr;  
 
-//  for( int i=0;i<4;i++){ 
-//       printf("%d",*(p+i));
-//  }
+// //  for( int i=0;i<4;i++){ 
+// //       printf("%d",*(p+i));
+// //  }
+ 
+// // } //way to print array elements using pointer
 
-// } //way to print array elements using pointer
 
+// // int main(){ 
+// //     int arr[3][3] ={{1,2,3},{4,5,6},{7,8,9}}; 
+// //     int sum=0;
+// //        for(int i=0;i<3;i++){ 
+// //          for( int j=0;j<3;j++){ 
+// //               sum=sum+arr[i][j];
 
-// int main(){ 
-//     int arr[3][3] ={{1,2,3},{4,5,6},{7,8,9}}; 
-//     int sum=0;
-//        for(int i=0;i<3;i++){ 
-//          for( int j=0;j<3;j++){ 
-//               sum=sum+arr[i][j];
+// //     //         printf("%d",arr[i][j]); 
+// //     //      }printf(" ");
+// //         }
+// //     printf("%d\n",sum);
+// //     sum=0;
+// //     } }  
 
-//     //         printf("%d",arr[i][j]); 
-//     //      }printf(" ");
-//         }
-//     printf("%d\n",sum);
-//     sum=0;
-//     } }  
-
-// i == 0 || n % (i + 2) == 0)
-//             return false;
-//     }
-//     return true;
-// }
+// // i == 0 || n % (i + 2) == 0)
+// //             return false;
+// //     }
+// //     return true;
+// // }
 
 
  
 
-// #include <stdio.h>
+// // #include <stdio.h>
 
-// int main() {
-//     int n, i, isPrime = 1;
-//     printf("Enter a number: ");
-//     scanf("%d", &n);
+// // int main() {
+// //     int n, i, isPrime = 1;
+// //     printf("Enter a number: ");
+// //     scanf("%d", &n);
 
-//     if (n <= 1) isPrime = 0;
-//     for (i = 2; i * i <= n; i++) {
-//         if (n % i == 0) {
-//             isPrime = 0;
-//             break;
-//         }
-//     }
+// //     if (n <= 1) isPrime = 0;
+// //     for (i = 2; i * i <= n; i++) {
+// //         if (n % i == 0) {
+// //             isPrime = 0;
+// //             break;
+// //         }
+// //     }
 
-//     if (isPrime)
-//         printf("%d is a prime number.\n", n);
-//     else
-//         printf("%d is not a prime number.\n", n);
+// //     if (isPrime)
+// //         printf("%d is a prime number.\n", n);
+// //     else
+// //         printf("%d is not a prime number.\n", n);
 
-//     return 0;
-// } 
+// //     return 0;
+// // } 
      
-int main(){ 
-     int a=3; 
-     printf
-}
-//
+// int main(){ 
+//      int a=3; 
+//      printf
+// }
+// //
