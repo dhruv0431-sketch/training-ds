64140b2231985ebe3f6921537f9ed149f16ee812
DHRUV PANDEY <panditdhruv0431@gmail.com>
2025-10-06 13:39:38 +0530

    v

--- .vscode/lec10.c
+++ .vscode/lec10.c
@@ -1,15 +1,28 @@
 #include<stdio.h> 
+#include<string.h>
  
-  int main(){ 
-     char name[ ] = " hii"; 
-     int i=0; 
+//   int main(){ 
+//      char name[ ] = " hii"; 
+//      int i=0; 
 
  
 
-  while (name[i]  != '\0') { 
-      printf("%c\n", name[i]);  
-        i++; 
-  }
+//   while (name[i]  != '\0') { 
+//       printf("%c\n", name[i]);  
+//         i++; 
+//   }
 
-  }
+//   }
    
+int main(){  
+    char name[] = " hii"; 
+    char data[10];
+     
+
+    for( int i=0;name[i]!='\0';i++){ 
+        printf("%c", name[i]); 
+        data[i]= name[i]; 
+    } 
+    printf("%s",data);
+
+}
