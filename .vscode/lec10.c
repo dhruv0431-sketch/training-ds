ebbe07e2311355bc5d16c7198dc635d224864bf7
DHRUV PANDEY <panditdhruv0431@gmail.com>
2025-10-06 14:02:00 +0530

    k

--- .vscode/lec10.c
+++ .vscode/lec10.c
@@ -14,15 +14,31 @@
 
 //   }
    
-int main(){  
-    char name[] = " hii"; 
-    char data[10];
+// int main(){  
+//     char name[] = " hii"; 
+//     char data[10];
      
 
-    for( int i=0;name[i]!='\0';i++){ 
-        printf("%c", name[i]); 
-        data[i]= name[i]; 
-    } 
-    printf("%s",data);
+//     for( int i=0;name[i]!='\0';i++){ 
+//         printf("%c", name[i]); 
+//         data[i]= name[i]; 
+//     } 
+//     printf("%s",data);
 
-}
+// }  
+ 
+int main(){ 
+     
+    char name[] =" madam"; 
+    int ln= strlen(name); 
+    int istrue=1; 
+    for( int i=0;i<ln/2;i++){ 
+        if(name[i] !=name[ln-i-1]){ 
+            istrue=0; 
+            break;
+        }
+    }   
+    printf("%s", name); 
+
+    }
+   
