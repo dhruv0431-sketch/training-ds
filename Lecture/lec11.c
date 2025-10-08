c09fef8bc181bd4797f8ba2849510ca48af02974
DHRUV PANDEY <panditdhruv0431@gmail.com>
2025-10-08 13:13:51 +0530

    j

---
+++ Lecture/lec11.c
@@ -0,0 +1,21 @@
+#include<stdio.h> 
+int main(){  
+    char str[] ="hello"; 
+    int ln=strlen( str); 
+    int count=0; 
+    for (int i =0;i<ln;i++){  
+        count=0; 
+        for( int j=0; j<i;j++)
+        {  if(str[i]==str[j]){ 
+            count++;
+            break;
+        } 
+    
+    
+        
+
+    }  if (count==0){ 
+        printf("%c",str[i]);
+    }
+    
+    }}
